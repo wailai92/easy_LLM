d3837c45d6eb9835257299094a583c011303945d
wailai92 <wayne.lai.902@gmail.com>
2026-04-20 15:03:19 +0800

    now support single-turn text generation and multi-turn conversational interaction by using the /api/generate and /api/chat endpoints

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import requests
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+OLLAMA_URL_chat = "http://localhost:11434/api/chat"
+OLLAMA_URL_generate = "http://localhost:11434/api/generate"
 MODEL_NAME = "gemma3:1b"
+
+def chat_llm(messages: list[dict]) -> str:
+    payload = {
+        "model": MODEL_NAME,
+        "messages": messages,
+        "stream": False
+    }
+
+    response = requests.post(OLLAMA_URL_chat, json=payload, timeout=120)
+    response.raise_for_status()
+
+    data = response.json()
+    return data["message"]["content"]
 
 def ask_llm(prompt: str) -> str:
     payload = {
@@ -10,20 +24,42 @@ def ask_llm(prompt: str) -> str:
         "stream": False
     }
 
-    response = requests.post(OLLAMA_URL, json=payload, timeout=120)
+    response = requests.post(OLLAMA_URL_generate, json=payload, timeout=120)
     response.raise_for_status()
 
     data = response.json()
     return data["response"]
 
 def main():
-    user_input = input("ask anything: ")
-    try:
-        reply = ask_llm(user_input)
-        print("\nresponse: ")
-        print(reply)
-    except:
-        print("ollama connection failed")
+    print("本地 LLM 聊天程式\n")
+    print("輸入 exit 離開\n")
+
+    messages = [
+        {
+            "role": "system",
+            "content": "一律使用繁體中文回答。"
+        }
+    ]
+    while True:
+        user_input = input("ask anything: ").strip()
+        if user_input == "exit":
+            break
+        if not user_input:
+            continue
+        messages.append({
+            "role": "user",
+            "content": user_input
+        })
+        try:
+            reply = chat_llm(messages)
+            print(f"reply： {reply}\n")
+
+            messages.append({
+                "role": "assistant",
+                "content": reply
+            })
+        except:
+            print("ollama connection failed")
 
 if __name__ == "__main__":
     main()
